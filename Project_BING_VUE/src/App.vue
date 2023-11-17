585c29596c97f715dfa70b6d34780407cf3037be
Jo hanbin <whgksqls9999@naver.com>
2023-11-18 01:13:37 +0900

    헤더 색,fixed 이동시 변경, 로고 배경색 변경

--- Project_BING_VUE/src/App.vue
+++ Project_BING_VUE/src/App.vue
@@ -5,7 +5,7 @@
       @click="doLoadingEnd"
     />
     <!-- loading -->
-    <div class="app-header">
+    <div class="app-header" :class="{fixed: isHeaderFixed}">
       <TheHeader class="router-header" />
     </div>
     <div class="app-routerview-container">
@@ -18,15 +18,22 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref,computed } from "vue";
 import { RouterView } from "vue-router";
 import TheHeader from "@/components/commons/TheHeader.vue";
 import TheFooter from "./components/commons/TheFooter.vue";
 import TheLoading from "./components/commons/TheLoading.vue";
+import { useCommonStore } from "@/stores/commonStore";
 
+// 헤더 fixed 상태
+const commonStore = useCommonStore();
+const isHeaderFixed = computed(() => commonStore.isHeaderFixed);
+
+// loading 클릭 시 꺼지게하기
 const loadingEnd = ref(true);
 const doLoadingEnd = () => {
   loadingEnd.value = !loadingEnd.value;
+  window.scrollTo(0,0);
 };
 </script>
 
@@ -34,17 +41,19 @@ const doLoadingEnd = () => {
 * {
   font-family: Poppins, "Noto Sans KR", sans-serif;
 }
+.fixed{
+  position: fixed;
+}
 .loading-end {
   display: none;
 }
 .app-loading {
   position: fixed;
-  background-color: white;
+  background-color: black;
   width: 100%;
   z-index: 101;
 }
 .app-header {
-  position: fixed;
   width: 100%;
   top: 0;
   left: 0;
--- Project_BING_VUE/src/components/commons/TheHeader.vue
+++ Project_BING_VUE/src/components/commons/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header-global">
-    <div class="header-container">
+    <div class="header-container" :class="{fixed: !isHeaderFixed}">
       <div class="weather">
         <TheWeather />
       </div>
@@ -8,7 +8,7 @@
         <p>HABING</p>
       </div>
       <div class="header-router-container">
-        <RouterLink :to="{ name: 'main' }">HOME</RouterLink>
+        <RouterLink :to="{ name: 'main' }" @click="scrollInit">HOME</RouterLink>
         <RouterLink :to="{ name: 'community' }">COMMUNITY</RouterLink>
         <button v-if="!loginUser" @click="changeForm(1)">SIGN IN</button>
         <button v-if="!loginUser" @click="changeForm(2)">SIGN UP</button>
@@ -28,16 +28,27 @@
 </template>
 
 <script setup>
+import { useCommonStore } from "../../stores/commonStore";
 import { useUserStore } from "../../stores/userStore";
 import { RouterLink } from "vue-router";
 import { ref, computed, onMounted } from "vue";
 import LoginForm from "@/components/account/LoginForm.vue";
 import RegistForm from "@/components/account/RegistForm.vue";
 import TheWeather from "@/components/commons/TheWeather.vue";
+
+const commonStore = useCommonStore();
 const userStore = useUserStore();
 const loginUser = computed(() => userStore.loginUser);
 const headerShow = ref(true);
 const footerShow = ref(false);
+
+// 헤더 fixed 상태
+const isHeaderFixed = computed(() => commonStore.isHeaderFixed);
+
+//스크롤 초기화
+const scrollInit = () => {
+  scrollTo(0,0);
+}
 // 페이지 로딩 시 로그인 유저 체크
 onMounted(() => {
   userStore.loginUser = sessionStorage.getItem("access-token");
@@ -58,6 +69,7 @@ a {
   text-decoration: none;
   color: white;
 }
+
 button {
   font-size: 17px;
   padding: 0;
@@ -107,5 +119,9 @@ button {
   align-items: center;
   font-size: 17px;
   font-weight: 500;
+}
+
+.fixed{
+  background-color: black;
 }
 </style>
