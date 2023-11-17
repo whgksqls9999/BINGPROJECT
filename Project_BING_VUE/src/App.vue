dad914b8db53b534b615b8452708fa8a2e9a8691
JoHanbin <whgksqls9999@naver.com>
2023-11-17 16:43:56 +0900

    css 작업 합치기

--- Project_BING_VUE/src/App.vue
+++ Project_BING_VUE/src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <!-- loading -->
-  <TheHeader />
-  <RouterView />
-  <!-- <Kakaotest /> -->
-  <!-- <Kakaotest2 /> -->
-  <!-- footer -->
+  <div class="app-global">
+    <!-- loading -->
+    <div class="app-header">
+      <TheHeader />
+    </div>
+    <div class="app-routerview-container">
+      <RouterView />
+    </div>
+    <div class="app-footer">
+      <TheFooter />
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
 import TheHeader from "@/components/commons/TheHeader.vue";
-import Kakaotest from "./components/kakaotest.vue";
-import Kakaotest2 from "./components/kakaotest2.vue";
+import TheFooter from "./components/commons/TheFooter.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+* {
+  font-family: Poppins, "Noto Sans KR", sans-serif;
+}
+.app-header {
+  position: sticky;
+  width: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+}
+.app-footer {
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+}
+.app-routerview-container {
+  /* position: relative; */
+  /* margin-top: 7vh; */
+  /* margin-bottom: -7vh;  */
+}
+</style>
