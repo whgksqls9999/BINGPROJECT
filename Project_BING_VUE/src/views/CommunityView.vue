d7cd366005b03800a397d72ac360035ab83fe304
Jo hanbin <whgksqls9999@naver.com>
2023-11-19 20:24:08 +0900

    커뮤니티 페이지 CSS

--- Project_BING_VUE/src/views/CommunityView.vue
+++ Project_BING_VUE/src/views/CommunityView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="comm-form">
-      <div class="comm-title">SELECT COMMUNITY</div>
+      <!-- <div class="comm-title"></div> -->
       <div class="comm-menu">
         <RouterLink :to="{ name: 'board', params: { community_id: '1' } }" class="freediving">FREEDIVING</RouterLink>
         <div class="freediving-desc desc">
@@ -11,6 +11,7 @@
         <div class="skinscuba-desc desc">
           WELCOME TO SKINSCUBA WORLD!
         </div>
+        <div class="comm-title"> SELECT COMMUNITY </div>
       </div>
     </div>
   </div>
@@ -28,74 +29,102 @@ body {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 5%;
+  margin-top: 2%;
   text-align: center;
 }
-
-.desc{
+*{
+  box-sizing: border-box;
+}
+.desc {
   position: absolute;
-  top: 200%;
+  top: 100%;
   margin-top: 1rem;
-  transition: all 1s ease;
+  transition: all 0.7s ease;
   opacity: 0;
   font-size: 2rem;
   width: 40rem;
+  background-color: white;
+  height: 20rem;
+  padding: 1rem;
+  text-align: left;
 }
 
-.freediving:hover +
-.freediving-desc,
-.skinscuba:hover +
+.freediving-desc{
+  border-top: 1rem solid black;
+  border-left: 1rem solid black;
+}
 .skinscuba-desc{
+  border-top: 1rem solid black;
+  border-left: 1rem solid black;
+}
+
+.freediving:hover+.freediving-desc,
+.skinscuba:hover+.skinscuba-desc {
   opacity: 1;
+  transform: translateY(1rem);
 }
 
-.comm-form{
-  box-shadow: 0 1px 0.3rem grey;
+.freediving:hover ~ .comm-title,
+.skinscuba:hover ~ .comm-title{
+  opacity: 0;
+  transform: translateY(-1rem);
+}
+
+.comm-form {
   width: 24rem;
-  padding: 1rem 0;
-  border-radius: 0.3rem;
 }
 
-.comm-title{
-  margin-bottom: 1rem;
-  font-size: 24px;
+.comm-title {
+  padding: 1rem;
+  text-align: left;
+  margin-top: 2rem;
+  border-top: 1rem solid black;
+  border-left: 1rem solid black;
+  top: 100%;
+  position: absolute;
+  margin-bottom: 0.2rem;
+  font-size: 50px;
   font-weight: bold;
+  width: 40rem;
+  height: 20rem;
+  opacity: 1;
+  transition: all 0.7s ease;
 }
 
-.comm-menu{
+.comm-menu {
   display: flex;
   justify-content: center;
-  gap: 4rem;
+  gap: 1rem;
   height: 100%;
   position: relative;
 }
 
-a{
+a {
   text-decoration: none;
   color: white;
   background-color: black;
   border-radius: 2rem;
   padding: 1rem;
-  transition: all 1s ease;
+  transition: all 0.8s ease;
   position: relative;
   opacity: 0.85;
 }
 
-a:hover{
-  background-color: black;
-  padding: 0.8rem;
+a:hover {
+  background-color: rgb(255, 255, 255);
+  color: black;
   opacity: 1;
-  font-size: 18px;
-  box-shadow: 0 0 1rem black;
+  box-shadow: 0 0 0.6rem black;
 }
 
-a:active{
-  background-color: black;  
-  font-size: 20px;
+a:active {
+  background-color: black;
+  /* font-size: 20px; */
   opacity: 0.85;
+  color: white;
 }
 
-*{
+* {
   box-sizing: border-box;
 }
 </style>
