ffc691d2c5da346076a4e50fd0844b10f8bd0b3b
김은지 <eungg0214@gmail.com>
2023-11-22 09:45:05 +0900

    registform 드디어 수정

--- Project_BING_VUE/src/components/account/RegistForm.vue
+++ Project_BING_VUE/src/components/account/RegistForm.vue
@@ -1,21 +1,51 @@
 <template>
-  <div class="regist-container">
+  <div class="regist-container" @click.stop="deleteClick">
     <div class="regist-form">
-      <label for="">email</label>
-      <input type="text" v-model="email" />
-      <label for="">pw</label>
-      <input type="text" v-model="pw" />
-      <label for="">pwCheck</label>
-      <input type="text" v-model="pwCheck" />
-      <label for="">name</label>
-      <input type="text" v-model="name" />
-      <label for="">gender</label>
-      <input type="text" v-model="gender" />
-      <label for="">nickname</label>
-      <input type="text" v-model="nickname" />
-      <label for="">withdraw text</label>
-      <input type="text" v-model="withdraw_text" />
-      <button @click="registUser">등록하기</button>
+      <h2></h2>
+      <form v-on:submit.prevent>
+        <div class="user-box">
+          <input type="text" v-model="email" />
+          <label for="">Id</label>
+        </div>
+        <div class="user-box">
+          <input type="text" v-model="pw" />
+          <label for="">pw</label>
+        </div>
+        <div class="user-box">
+          <input type="text" v-model="pwCheck" />
+          <label for="">pwCheck</label>
+        </div>
+        <div class="user-box">
+          <input type="text" v-model="name" />
+          <label for="">name</label>
+        </div>
+        <div class="user-box">
+          <input type="text" v-model="gender" />
+          <label for="">gender</label>
+        </div>
+        <div class="user-box">
+          <input type="text" v-model="nickname" />
+          <label for="">nickname</label>
+        </div>
+        <div class="user-box">
+          <input type="text" v-model="withdraw_text" />
+          <label for="">withdraw text</label>
+        </div>
+        <div class="regist-button">
+          <button @click="registUser">
+            <span></span>
+            <span></span>
+            <span></span>
+            등록하기
+          </button>
+          <button @click="closeWindow">
+            <span></span>
+            <span></span>
+            <span></span>
+            취소하기
+          </button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -53,6 +83,26 @@ const passwordConfirm = () => {
 const isPwValid = (pwd) => {
   return pwd.length >= 8 && /[!@#$%^&*(),.?":{}|<>]/g.test(pwd);
 };
+
+const closeWindow = () => {
+  userStore.changeForm(0);
+}
+
+const deleteClick = (e) => {
+  if (e.target.className === "regist-container") {
+    closeWindow();
+  }
+}
+
+// const cancleRegist = () =>{
+//   email.value === "",
+//   pw.value === "",
+//   name.value === "",
+//   gender.value === "",
+//   nickname.value === "",
+//   withdraw_text.value === "",
+//   cancleRegist();
+// }
 
 const registUser = async () => {
   if (
@@ -100,7 +150,7 @@ const registUser = async () => {
 </script>
 
 <style scoped>
-.regist-container{
+/* .regist-container {
   width: 100%;
   height: 100vh;
   position: fixed;
@@ -109,12 +159,195 @@ const registUser = async () => {
   left: 0;
 }
 
-.regist-form{
+.regist-form {
   display: flex;
   flex-direction: column;
   width: 20rem;
   position: fixed;
   top: 15rem;
   left: 20rem;
+} */
+.regist-container {
+  background-color: rgba(128, 128, 128, 0.4);
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  z-index: 1;
+}
+.regist-form {
+  margin-top: 250px;
+  position: fixed;
+  top: 10%;
+  left: 50%;
+  width: 400px;
+  padding: 40px;
+  transform: translate(-50%, -50%);
+  background: rgba(0, 0, 0, 0.7);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
+  border-radius: 10px;
+  z-index: 2;
+}
+
+.regist-form h2 {
+  margin: 0 0 30px;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+}
+
+.regist-form .user-box {
+  position: relative;
+}
+
+.regist-form .user-box input {
+  width: 100%;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 1px solid #fff;
+  outline: none;
+  background: transparent;
+}
+.regist-form .user-box label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  pointer-events: none;
+  transition: 0.5s;
+}
+
+.regist-form .user-box input:focus ~ label,
+.regist-form .user-box input:valid ~ label {
+  top: -20px;
+  left: 0;
+  color: #b6eaec;
+  font-size: 12px;
+}
+
+.regist-form form button {
+  position: relative;
+  display: inline-block;
+  padding: 10px 20px;
+  color: #c0cdce;
+  font-size: 16px;
+  text-decoration: none;
+  text-transform: uppercase;
+  overflow: hidden;
+  transition: 0.5s;
+  margin-top: 40px;
+  letter-spacing: 4px;
+}
+
+.regist-form button:hover {
+  background: #c0cdce;
+  color: #fff;
+  border-radius: 5px;
+  border: 1px solid transparent;
+  box-shadow: 0 0 5px #c0cdce, 0 0 25px #c0cdce, 0 0 50px #c0cdce,
+    0 0 100px #c0cdce;
+}
+
+.regist-form button span {
+  position: absolute;
+  display: block;
+}
+
+.regist-form button span:nth-child(1) {
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, transparent, #8db4b6);
+  animation: btn-anim1 1s linear infinite;
+}
+
+@keyframes btn-anim1 {
+  0% {
+    left: -100%;
+  }
+  50%,
+  100% {
+    left: 100%;
+  }
+}
+
+.regist-form button span:nth-child(2) {
+  top: -100%;
+  right: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(180deg, transparent, #8db4b6);
+  animation: btn-anim2 1s linear infinite;
+  animation-delay: 0.25s;
+}
+
+@keyframes btn-anim2 {
+  0% {
+    top: -100%;
+  }
+  50%,
+  100% {
+    top: 100%;
+  }
+}
+
+.regist-form button span:nth-child(3) {
+  bottom: 0;
+  right: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(270deg, transparent, #8db4b6);
+  animation: btn-anim3 1s linear infinite;
+  animation-delay: 0.5s;
+}
+
+@keyframes btn-anim3 {
+  0% {
+    right: -100%;
+  }
+  50%,
+  100% {
+    right: 100%;
+  }
+}
+
+.regist-form button span:nth-child(4) {
+  bottom: -100%;
+  left: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(360deg, transparent, #8db4b6);
+  animation: btn-anim4 1s linear infinite;
+  animation-delay: 0.75s;
+}
+
+@keyframes btn-anim4 {
+  0% {
+    bottom: -100%;
+  }
+  50%,
+  100% {
+    bottom: 100%;
+  }
+}
+
+.regist-button {
+  display: flex;
+  justify-content: space-around;
+}
+
+button {
+  border: 1px solid transparent;
+  background-color: transparent;
+  border-style: none;
+  color: white;
+  font-family: "Noto Sans KR";
 }
 </style>
