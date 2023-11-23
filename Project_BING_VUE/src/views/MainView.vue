cdae733c8e253b81800ba461f7d1c31dcaff8c89
Jo hanbin <whgksqls9999@naver.com>
2023-11-23 23:51:56 +0900

    메인 하는 중

--- Project_BING_VUE/src/views/MainView.vue
+++ Project_BING_VUE/src/views/MainView.vue
@@ -6,7 +6,23 @@
       <div class="p1-e1">해빙</div>
       <div class="p1-e2"></div>
       <div class="p1-e3">解氷</div>
+      <div class="p1-e4"></div>
+      <!-- 얼음이 녹아 풀리는 것 -->
+      <!--  -->
     </div>
+    <div class="p2">
+      <div class="p2-e2"></div>
+      <div class="p2-e1">
+        얼음이 녹아 풀리는 것
+      </div>
+    </div>
+    <div class="p3"></div>
+    <div class="p4">
+      <div class="p4-e1">
+        <span>asdf</span>
+      </div>
+    </div>
+    <div class="p5"></div>
   </div>
 </template>
 
@@ -20,19 +36,88 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 onMounted(() => {
-  gsap.to(".a1,.a2", {
+  window.addEventListener('click', (e) => {
+    console.log(e.offsetY);
+  })
+  
+  gsap.to(".p1-e1", {
     scrollTrigger: {
-      trigger: ".a1",
+      trigger: ".p1-e1",
       toggleActions: "restart none reverse none",
       start: "100% 80%",
       end: "0% 20%",
-      scrub: 2,
+      scrub: 5,
       opacity: 0,
       markers: true,
     },
     opacity: 1,
     x: 20,
   });
+  gsap.to(".p1-e2", {
+    scrollTrigger: {
+      trigger: ".p1-e1",
+      toggleActions: "restart none reverse none",
+      start: "100% 80%",
+      end: "0% 20%",
+      scrub: 3,
+      opacity: 0,
+      markers: true,
+      width:400
+    },
+    opacity: 1,
+    width: 900,
+    x:20
+  });
+  gsap.to(".p1-e3", {
+    scrollTrigger: {
+      trigger: ".p1-e4",
+      toggleActions: "restart none reverse none",
+      start: "100% 80%",
+      end: "0% 20%",
+      scrub: 6,
+      opacity: 0,
+      markers: true,
+      width:400
+    },
+    opacity: 1,
+    x:-1
+  });
+  gsap.to(".p2-e1", {
+    scrollTrigger: {
+      trigger: ".p2",
+      toggleActions: "restart none reverse none",
+      start: "100% 80%",
+      end: "0% 20%",
+      scrub: 1,
+      markers: true,
+    },
+    opacity: 1,
+    x: -3
+  });
+  gsap.to(".p2-e1", {
+    scrollTrigger: {
+      trigger: ".p3",
+      toggleActions: "restart none reverse none",
+      start: "100% 80%",
+      end: "0% 20%",
+      scrub: 0,
+      markers: true,
+    },
+    y: -50
+  });
+  gsap.to(".p4-e1", {
+    scrollTrigger: {
+      trigger: ".p5",
+      toggleActions: "restart none reverse none",
+      start: "100% 80%",
+      end: "0% 20%",
+      scrub: 0,
+      markers: true,
+    },
+    y: -50
+  });
+
+
 });
 // let options = {
 //   root: document.querySelector('#scrollArea'),
@@ -51,9 +136,6 @@ onMounted(() => {
 // const isVisible = ref(false);
 // observer.stop
 
-window.addEventListener('scroll', function () {
-  console.log(window.scrollY)
-});
 
 // # init
 // 헤더 fixed toggle
@@ -73,16 +155,79 @@ onMounted(() => {
 </script>
 
 <style scoped>
+*{
+  margin: 0;
+  padding: 0;
+}
 .body {
-  height: 5000px;
+  height: 10000px;
   background: linear-gradient(-7deg, rgb(180, 202, 221), rgb(0, 0, 0));
 }
 
-p1{
-  position: absolute;
+.p1{
+  position: sticky;
   color: white;
-  font-size: 40px;
-  top: 1000px;
+  font-weight: bold;
+  margin-top: 800px;
+  top: 50px;
+  margin-left: 100px;
+}
+.p1-e1{
+  font-size: 200px;
+  opacity: 0;
+}
+.p1-e2{
+  background-color: white;
+  height: 10px;
+  width: 200px;
+  margin-top: -31px;
+  opacity: 0;
+}
+.p1-e3{
+  font-size: 60px;
+  margin-left: 760px;
+  opacity: 0;
+}
+.p1-e4{
 
+}
+.p2{
+  color: white;
+  position: sticky;
+  top: 450px;
+  margin-left: 1200px;
+  margin-top: 800px;
+  overflow: hidden;
+}
+
+.p2-e1{
+  font-size: 40px;
+  opacity: 0;
+}
+
+.p3{
+ margin-top: 900px;
+}
+.p4{
+  position: fixed;
+  position: relative;
+  left: 1200;
+  top: 400;
+  opacity: 1;
+  height: 50px;
+  width: 100%;
+  opacity: 1;
+  z-index: 1;
+}
+.p4-e1{
+  position: fixed;
+  left: 1200;
+  top: 450;
+  opacity: 1;
+  height: 100px;
+  width: 100%;
+  opacity: 1;
+  font-size: 40px;
+  color: white;
 }
 </style>
